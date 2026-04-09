68cdd3cdb857c329ce685e1863f505c2f3c9ff2d
m01149 <Jiyong.Liu@metax-tech.com>
2026-04-09 15:53:46 +0800

    [fix][MC3-8379]default disable qwen3 alt cuda stream
    Change-Id: I4176695733c19cef3ee2db2d4cd2e96f77ddb009

--- python/sglang/srt/models/qwen3_next.py
+++ python/sglang/srt/models/qwen3_next.py
@@ -65,6 +65,9 @@ _is_npu = is_npu()
 _is_cpu = is_cpu()
 _is_amx_available = cpu_has_amx_support()
 
+import os
+
+SGLANG_QWEN3_5_ENABLE_ALT_STREAM = (os.getenv("SGLANG_QWEN3_5_ENABLE_ALT_STREAM", "0") == "1")
 
 class Qwen3GatedDeltaNet(nn.Module):
     def __init__(
@@ -775,7 +778,7 @@ class Qwen3NextModel(nn.Module):
         super().__init__()
         self.config = config
 
-        alt_stream = torch.cuda.Stream() if _is_cuda else None
+        alt_stream = torch.cuda.Stream() if _is_cuda and SGLANG_QWEN3_5_ENABLE_ALT_STREAM else None
 
         self.embed_tokens = VocabParallelEmbedding(
             config.vocab_size,
