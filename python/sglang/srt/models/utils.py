a5052b29fe5828b62f3a7b3e704e51ad863a8f6c
m01305 <chongzhou.shao@metax-tech.com>
2026-05-21 18:39:13 +0800

    [fix] Disable multi-stream operation in apply_qk_norm
    Change-Id: Ia671484a2273a1d49eb9b342c5e9bf9519208c2f

--- python/sglang/srt/models/utils.py
+++ python/sglang/srt/models/utils.py
@@ -434,20 +434,11 @@ def apply_qk_norm(
         )
         return q, k
 
-    if alt_stream is not None and get_is_capture_mode():
-        current_stream = get_current_device_stream_fast()
-        alt_stream.wait_stream(current_stream)
-        q_by_head = q.reshape(-1, head_dim)
-        q_by_head = q_norm(q_by_head)
-        with torch.cuda.stream(alt_stream):
-            k_by_head = k.reshape(-1, head_dim)
-            k_by_head = k_norm(k_by_head)
-        current_stream.wait_stream(alt_stream)
-    else:
-        q_by_head = q.reshape(-1, head_dim)
-        q_by_head = q_norm(q_by_head)
-        k_by_head = k.reshape(-1, head_dim)
-        k_by_head = k_norm(k_by_head)
+
+    q_by_head = q.reshape(-1, head_dim)
+    q_by_head = q_norm(q_by_head)
+    k_by_head = k.reshape(-1, head_dim)
+    k_by_head = k_norm(k_by_head)
     q = q_by_head.view(q.shape)
     k = k_by_head.view(k.shape)
     return q, k
